a742cf6bb6509974f43d2fff4769f259a6d74331
usver <z-ybr@mail.ru>
2025-05-23 09:12:40 +1000

    cli argument

---
+++ main.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+import click
+
+SAMPLE_PATH="~/"
+OUT_FILE="out.txt"
+
+def get_pdf_from_path(path_to_scan, recursive, filename):
+    pth=path_to_scan
+    if recursive:
+        print("Scan with recursive...")
+    else:
+        print("Scan without recursive...")
+
+    return pth
+
+@click.command(help="Scan *.pdf file in LOCATION and output result in file.")
+#@click.argument('location')
+@click.argument(
+    "location",
+    type=click.Path(
+        exists=True,
+        file_okay=False,
+        readable=True,
+        path_type=Path,
+    ),
+)
+@click.option(
+    '--recursive', '-r', is_flag=True,
+    help = 'Scan subfolder in LOCATION',
+)
+@click.option(
+    '--filename', '-f',
+    prompt='Out filename',
+    default=OUT_FILE,
+    help='Filename for output scan result.',
+)
+@click.version_option("0.1.0", prog_name="sinpdf")
+def main(location, recursive, filename):
+    """
+        Search in PDF.
+        Scan *.pdf file in LOCATION and output result in file
+    """
+    target_dir = Path(location)
+    if not target_dir.exists():
+        click.echo("The target directory doesn't exist")
+        raise SystemExit(1)
+
+    for entry in target_dir.iterdir():
+        click.echo(f"{entry.name:{len(entry.name) + 5}}", nl=False)
+    click.echo()
+
+    find_count=get_pdf_from_path(location, recursive, filename)
+    print(f"Search {find_count} PDF file in {location}.")
+    print(f"Ouptput result in {filename}")
+
+if __name__ == '__main__':
+    main()
