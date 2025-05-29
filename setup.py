dd628634ca283add58f2e9c89e5ca1c06828692e
usver <z-ybr@mail.ru>
2025-05-29 11:39:33 +1000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sinpdf',
+    version='0.1',
+    description='Small program for scan PDF files massive and fast search text in result.',
+    license='MIT',
+    packages=['sinpdf'],
+    author='Rifgy',
+    author_email='z-ybr@mail.ru',
+    keywords=['sqlite', 'pdf-viewer', 'pdf'],
+    url='https://github.com/Rifgy/sinpdf.git'
+)
