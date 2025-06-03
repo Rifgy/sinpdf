3f7ee1ae6e51ed3ac3b215d10042e9b9092169a6
z-ybr <z-ybr@mail.ru>
2025-06-03 10:23:54 +1000

    add get_files_from_path.py

---
+++ test/get_files_from_path.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+def get_files_from_path(pathtodir: str) -> int:
+
+    if pathtodir:
+        target_dir = Path(pathtodir)
+        pdf_files = list(target_dir.rglob('*.pdf'))  # Получаем список всех PDF-файлов
+
+        for index, entry in enumerate(pdf_files):
+            print(f"{entry.name}\n\t\t{entry.as_posix()}")
+            index += 1
+        return len(pdf_files)
+    else:
+        print('Select folder error', 'Please select directory with files')
+        return 0
+
+if __name__ == "__main__":
+
+    #pth = "Y:\\146 Канцелярия\\Общая\\Приказы"
+    #pth = "Y:\\146 Канцелярия\\Общая\\Распоряжения"
+    #pth = "Y:\\146 Канцелярия\\Общая"
+    pth = "Y:/146 Канцелярия/Общая/CD/"
+    print(f"File count: {get_files_from_path(pth)}")
